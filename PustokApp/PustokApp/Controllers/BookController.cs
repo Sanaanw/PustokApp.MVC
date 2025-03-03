2949c16478e469e42febe54fc6968d1f8389f146
Nesrullayev <senan.nesrullayevv@gmail.com>
2025-03-03 11:32:23 +0400

    Refactor1 commit

---
+++ PustokApp/PustokApp/Controllers/BookController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PustokApp.Data;
+using PustokApp.Models.Home;
+using PustokApp.ViewModels;
+
+namespace PustokApp.Controllers
+{
+    public class BookController(PustokAppContext context) : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+
+        public IActionResult Detail(int? Id)
+        {
+            if (Id == null)
+                return NotFound();
+            var ExistBook = context.Book
+                .Include(b => b.Brand)
+                .Include(a => a.Author)
+                .Include(bi => bi.BookImages)
+                .Include(Bt => Bt.BookTags)
+                .ThenInclude(t => t.Tag)
+                .FirstOrDefault(y => y.Id == Id);
+            if (ExistBook == null)
+                return NotFound();
+
+            BookDetailVm bookDetailVm = new()
+            {
+                Book = ExistBook,
+                ReleatedBook = context.Book
+                .Include(b => b.Brand)
+                .Include(a => a.Author)
+                .Include(bi => bi.BookImages)
+                .Where(x=>x.AuthorId==ExistBook.AuthorId)
+                .ToList()
+            };
+
+            return View(bookDetailVm);
+        }
+    }
+}
---
+++ PustokApp/PustokApp/ViewModels/BookDetailVm.cs
@@ -0,0 +1,10 @@
+﻿using PustokApp.Models.Home;
+
+namespace PustokApp.ViewModels
+{
+    public class BookDetailVm
+    {
+        public Book Book { get; set; }
+        public List<Book> ReleatedBook { get; set; }
+    }
+}
